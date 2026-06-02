e9d5ad7980b4dc220031cc47eb2fcdf0ee949c49
lucasescouto-ux <lucas.escouto@saipos.com>
2026-06-01 22:47:01 -0300

    Aula #182. MY21 - Consultando informações do menu administrativo no MySQL

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -29,8 +29,16 @@ router.get('/logout', function(req, res, next){
 
 router.get('/', function (req, res, next){
 
-    res.render('admin/index', admin.getParams(req));
-    
+    admin.dashboard().then(data => {
+
+        res.render('admin/index', admin.getParams(req, {
+            data
+        }));
+
+    }).catch(err=>{
+
+        console.log(err);
+    });
 });
 
 router.post('/login', function (req, res, next){
