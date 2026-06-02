53f3a6e8528f484a428a400c83440d597921e28c
lucasescouto-ux <lucas.escouto@saipos.com>
2026-06-01 23:04:31 -0300

    Aula #183. MY22 - Consultando dados do menu no MYSQL

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var users = require("./../inc/users");
 var admin = require("./../inc/admin");
+var menus = require("./../inc/menu")
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -78,7 +79,12 @@ router.get('/emails', function (req, res, next){
 
 router.get('/menus', function (req, res, next){
 
-    res.render('admin/menus', admin.getParams(req));
+    menus.getMenus().then(data => {
+
+        res.render('admin/menus', admin.getParams(req, {
+            data
+        }));
+    });
 });
 
 router.get('/reservations', function (req, res, next){
